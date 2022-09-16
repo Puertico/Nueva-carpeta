e9ff43ea625e87d7584b34211743c90d89df7fb9
Cristian Puerto <capuetoc@correo.udistrital.edu.com>
2022-09-16 17:04:37 -0500

    v 0.03

--- programa.py
+++ programa.py
@@ -4,33 +4,32 @@ import random
 class Cuestionador:
 
     def __init__(self):
-
         self.questions=[
-            "¿Qué es el ALMICANTARAT?"
-            "¿Dónde se encuentra el Zenit?"
+            "¿Qué es el ALMICANTARAT?",
+            "¿Dónde se encuentra el Zenit?",
             "¿Cuántos órdenes exsisten en la taxonamia de sueles?"
         ]
 
         self.answers=[
-            "Circulo imaginario en esfera celeste"
-            "90 grados respecto al horizonte"
-            12
+            "Circulo imaginario en esfera celeste",
+            "90 grados respecto al horizonte",
+            "12"
         ]
 
-def jugar(self):
-    #generar un número aleatorio entre 0 y n-1 siendo n el
-    # tamaño de la lista de pregunta
-    n=len(self.questionns)
-    number= random.randint(0, n-1)
-    print(self.questions[number])
+    def jugar(self):
+        #generar un número aleatorio entre 0 y n-1 siendo n el
+        # tamaño de la lista de pregunta
+        n=len(self.questions)
+        number= random.randint(0, n-1)
+        print(self.questions[number])
 
-    #solicitar la respuesta al usuario
-    respuesta=imput("¿Cuál es tu respuesta?")
+        #solicitar la respuesta al usuario
+        respuesta=input("¿Cuál es tu respuesta?")
 
-    #verificar si la respuesta es correcta
+        #verificar si la respuesta es correcta
 
-    if respuesta== self.answers[number]:
-        print("Eres genial!!")
-    else:
-        print("Deje de pensar que sus papás lo van a mantener toda la vida") 
+        if respuesta== self.answers[number]:
+            print("Eres genial!!")
+        else:
+            print("Deje de pensar que sus papás lo van a mantener toda la vida") 
 
