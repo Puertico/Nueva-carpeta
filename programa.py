f0ad354eea468c33ee84ef3cb1746747823364a7
Cristian Puerto <capuetoc@correo.udistrital.edu.com>
2022-09-16 16:39:33 -0500

    v 0.01

---
+++ programa.py
@@ -0,0 +1,18 @@
+
+
+
+class Cuestionador:
+
+    def __init__(self):
+
+        self.questions=[
+            "¿Qué es el ALMICANTARAT?"
+            "¿Dónde se encuentra el Zenit?"
+            "¿Cuántos órdenes exsisten en la taxonamia de sueles?"
+        ]
+
+        self.answers=[
+            "Circulo imaginario en esfera celeste"
+            "90 grados respecto al horizonte"
+            12
+        ]
