2d6e5a660542e8dde96df103196ead843f14715d
Cristian Puerto <capuetoc@correo.udistrital.edu.com>
2022-09-16 16:56:46 -0500

    v 0.02

--- programa.py
+++ programa.py
@@ -1,5 +1,5 @@
 
-
+import random
 
 class Cuestionador:
 
@@ -16,3 +16,21 @@ class Cuestionador:
             "90 grados respecto al horizonte"
             12
         ]
+
+def jugar(self):
+    #generar un número aleatorio entre 0 y n-1 siendo n el
+    # tamaño de la lista de pregunta
+    n=len(self.questionns)
+    number= random.randint(0, n-1)
+    print(self.questions[number])
+
+    #solicitar la respuesta al usuario
+    respuesta=imput("¿Cuál es tu respuesta?")
+
+    #verificar si la respuesta es correcta
+
+    if respuesta== self.answers[number]:
+        print("Eres genial!!")
+    else:
+        print("Deje de pensar que sus papás lo van a mantener toda la vida") 
+
